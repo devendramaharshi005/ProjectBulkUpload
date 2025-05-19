5e5f5329b48f7bf1cb22e1f63a9991f8cf16e0b7
Devendra Maharshi <devendra.maharshi@celebaltech.com>
2025-05-20 01:39:44 +0530

    change in the test route.

--- bulkdata/src/main/java/com/example/demo/controller/HomeController.java
+++ bulkdata/src/main/java/com/example/demo/controller/HomeController.java
@@ -1,17 +1,18 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/")
 public class HomeController {
 
-    @GetMapping("/")
-    public Map<String, String> home() {
-        Map<String, String> response = new HashMap<>();
-        response.put("message", "Hello, it is a test route");
-        return response;
-    }
+	 @GetMapping
+	    public Map<String, String> home() {
+	        Map<String, String> response = new HashMap<>();
+	        response.put("message", "Hello, it is a test route");
+	        return response;
+	   }
 }
